e5c58a80643f55c4da3183ce73ed1d01c3d770d8
howardsolutions <phungthinh0946519356@gmail.com>
2025-02-07 17:23:39 -0700

    promisify solution

--- advance_node_excercises/Exercise Files/Ch01/01_04/start/index.js
+++ advance_node_excercises/Exercise Files/Ch01/01_04/start/index.js
@@ -1,18 +1,40 @@
-var delay = (seconds, callback) => {
-    if (seconds > 3) {
-        callback(new Error(`${seconds} seconds it too long!`));
-    } else {
-        setTimeout(() =>
-            callback(null, `the ${seconds} second delay is over.`),
-            seconds
-        );
-    }
+// var { promisify } = require('util');
+
+function promisify(fn) {
+  return function(...args) {
+    return new Promise((resolve, reject) => {
+      function customCallback(err, result) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      }
+
+      fn.call(this, ...args, customCallback);
+    });
+  };
 }
 
-delay(2, (error, message) => {
-    if (error) {
-        console.log(error.message);
-    } else {
-        console.log(message);
-    }
-});
+var delay = (seconds, callback) => {
+  if (seconds > 3) {
+    callback(new Error(`${seconds} seconds it too long!`));
+  } else {
+    setTimeout(
+      () => callback(null, `the ${seconds} second delay is over.`),
+      seconds
+    );
+  }
+};
+
+// delay(2, (error, message) => {
+//   if (error) {
+//     console.log(error.message);
+//   } else {
+//     console.log(message);
+//   }
+// });
+
+const promiseDelay = promisify(delay);
+
+promiseDelay(2).then(console.log).catch(console.error);
