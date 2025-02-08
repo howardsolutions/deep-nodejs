a2f89f5083f649f01799ca1ca25d8ed9d91a60f9
howardsolutions <phungthinh0946519356@gmail.com>
2025-02-07 17:05:27 -0700

    feat: promise solution

--- advance_node_excercises/Exercise Files/Ch01/01_02/start/index.js
+++ advance_node_excercises/Exercise Files/Ch01/01_02/start/index.js
@@ -1,9 +1,19 @@
-function delay(seconds, callback) {
-    setTimeout(callback, seconds*1000);
+// function delay(seconds, callback) {
+//     setTimeout(callback, seconds*1000);
+// }
+
+// delay(1, () => {
+//     console.log('one second');
+// })
+
+// console.log('end first tick');
+
+function delay(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, ms);
+  });
 }
 
-delay(1, () => {
-    console.log('one second');
-})
-
-console.log('end first tick');
+delay(1000)
+  .then(() => console.log('1 sec passed'))
+  .then(() => console.log('haha'));
