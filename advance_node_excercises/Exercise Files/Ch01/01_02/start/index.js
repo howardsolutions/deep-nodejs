d8e5970eb65a88439238807fbee350f0a7a7ef43
howardsolutions <phungthinh0946519356@gmail.com>
2025-02-07 17:07:46 -0700

    feat: reject the promise solution

--- advance_node_excercises/Exercise Files/Ch01/01_02/start/index.js
+++ advance_node_excercises/Exercise Files/Ch01/01_02/start/index.js
@@ -10,6 +10,9 @@
 
 function delay(ms) {
   return new Promise((resolve, reject) => {
+    if (ms > 3000) {
+      reject(new Error(`${ms} is too long!`));
+    }
     setTimeout(resolve, ms);
   });
 }
@@ -17,3 +20,7 @@ function delay(ms) {
 delay(1000)
   .then(() => console.log('1 sec passed'))
   .then(() => console.log('haha'));
+
+delay(4000)
+  .then(() => console.log('1 sec passed'))
+  .catch((errMessage) => console.log(errMessage));
