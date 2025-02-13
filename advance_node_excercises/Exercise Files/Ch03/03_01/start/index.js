91f8a6e3bb0254b5e96b6a0dd88e018fd143273f
howardsolutions <phungthinh0946519356@gmail.com>
2025-02-13 14:58:50 -0700

    solving Range request in streaming

--- advance_node_excercises/Exercise Files/Ch03/03_01/start/index.js
+++ advance_node_excercises/Exercise Files/Ch03/03_01/start/index.js
@@ -10,12 +10,29 @@ const fileName = '../../../powder-day.mp4';
 const server = createServer(async (req, res) => {
   const { size } = await getFileInfo(fileName);
 
-  res.writeHead(200, {
-    'content-type': 'video/mp4',
-    'content-length': size,
-  });
-  
-  createReadStream(fileName).pipe(res);
+  const range = req.rawHeaders.range;
+
+  if (!range) {
+    res.writeHead(200, {
+      'content-type': 'video/mp4',
+      'content-length': size,
+    });
+
+    createReadStream(fileName).pipe(res);
+  } else {
+    let [start, end] = range.replace(/bytes=/, '').split('-');
+    start = parseInt(start, 10);
+    end = end ? parseInt(end, 10) : size - 1;
+
+    res.writeHead(206, {
+      'content-length': end - start + 1,
+      'content-range': `bytes ${start}-${end}/${size}`,
+      'accept-ranges': 'bytes',
+      'content-type': 'video/mp4',
+    });
+
+    createReadStream(fileName, { start, end }).pipe(res);
+  }
 });
 
 server.listen(3000, () => {
