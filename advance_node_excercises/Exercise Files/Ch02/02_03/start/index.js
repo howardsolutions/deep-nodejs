127bd3dd3aebb6571b602ae521d4ffa864a75fdd
howardsolutions <phungthinh0946519356@gmail.com>
2025-02-12 17:41:37 -0700

    Feat: using readable stream

---
+++ advance_node_excercises/Exercise Files/Ch02/02_03/start/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+const readStream = fs.createReadStream('../../powder-day.mp4');
+
+readStream.on('data', (chunk) => {
+  console.log('reading little chunk\n', chunk);
+});
+
+readStream.on('end', () => console.log('done'));
+
+readStream.on('error', (error) => console.log('Error has occured', error));
+
+// pause from flowing mode.
+readStream.pause();
+
+process.stdin.on('data', (chunk) => {
+  const text = chunk.toString().trim();
+
+  if (text == 'finish') {
+    // continue go back to the normal flowing mode
+    readStream.resume();
+  }
+});
