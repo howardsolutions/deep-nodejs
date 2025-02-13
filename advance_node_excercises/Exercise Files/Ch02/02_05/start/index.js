22eb510c6eb9b2886b9cb460c8b6bf367e7f509e
howardsolutions <phungthinh0946519356@gmail.com>
2025-02-12 18:12:04 -0700

    Pipe values from readable stream to writable stream

--- advance_node_excercises/Exercise Files/Ch02/02_05/start/index.js
+++ advance_node_excercises/Exercise Files/Ch02/02_05/start/index.js
@@ -1,20 +1,34 @@
 const { createReadStream, createWriteStream } = require('fs');
 
 const readStream = createReadStream('../../powder-day.mp4');
-const writeStream = createWriteStream('./copy.mp4');
+
+const writeStream = createWriteStream('./copy.mp4', {
+  highWaterMark: 1628920,
+});
 
 readStream.on('data', (chunk) => {
-    writeStream.write(chunk);
+  const isCanHandleMoreData = writeStream.write(chunk);
+
+  if (!isCanHandleMoreData) {
+    console.log('Has backpressure');
+    readStream.pause();
+  }
 });
 
 readStream.on('error', (error) => {
-    console.log('an error occurred', error.message);
+  console.log('an error occurred', error.message);
 });
 
 readStream.on('end', () => {
-    writeStream.end();
+  writeStream.end();
 });
 
 writeStream.on('close', () => {
-    process.stdout.write('file copied\n');
-})
+  process.stdout.write('file copied\n');
+});
+
+// to know when we could start pouring data in again. when writeStream drain.
+writeStream.on('drain', () => {
+  console.log('DRAIN');
+  readStream.resume();
+});
