5534b49539e37fab57509ddca9f52bcc39e1e333
howardsolutions <phungthinh0946519356@gmail.com>
2025-02-12 17:10:05 -0700

    feat: Readable streams from array

--- advance_node_excercises/Exercise Files/Ch02/02_02/start/index.js
+++ advance_node_excercises/Exercise Files/Ch02/02_02/start/index.js
@@ -1,9 +1,46 @@
+const { Readable } = require('stream');
+
 const peaks = [
-    "Tallac",
-    "Ralston",
-    "Rubicon",
-    "Twin Peaks",
-    "Castle Peak",
-    "Rose",
-    "Freel Peak"
+  'Tallac',
+  'Ralston',
+  'Rubicon',
+  'Twin Peaks',
+  'Castle Peak',
+  'Rose',
+  'Freel Peak',
 ];
+
+class StreamFromArray extends Readable {
+  constructor(array) {
+    // super({ encoding: 'utf-8' }); => read data in string format
+    super({ objectMode: true }); // can read any type of js object
+    this.array = array;
+
+    this.index = 0;
+  }
+
+  _read() {
+    if (this.index > this.array.length - 1) {
+      // push null to mark end of the stream
+      // otherwise it will run forever
+      return this.push(null);
+    }
+
+    // const chunk = this.array[this.index];
+    const chunk = {
+      data: this.array[this.index],
+      index: this.index,
+    };
+    // push chunk of data into stream
+    this.push(chunk);
+    this.index++;
+  }
+}
+
+const peakStream = new StreamFromArray(peaks);
+
+peakStream.on('data', (chunk) => {
+  console.log(chunk);
+});
+
+peakStream.on('end', () => console.log('done'));
