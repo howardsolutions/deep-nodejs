05832a41cd46afd613b5d6aee41a7ddac45c2672
howardsolutions <phungthinh0946519356@gmail.com>
2025-02-13 10:04:38 -0700

    Duplex streams

--- advance_node_excercises/Exercise Files/Ch02/02_07/start/index.js
+++ advance_node_excercises/Exercise Files/Ch02/02_07/start/index.js
@@ -1,6 +1,38 @@
+const { PassThrough, Duplex } = require('stream');
+
 const { createReadStream, createWriteStream } = require('fs');
 
-const readStream = createReadStream('../../powder-day.mp4')
+const readStream = createReadStream('../../powder-day.mp4');
 const writeStream = createWriteStream('./copy.mp4');
 
-readStream.pipe(writeStream);
+class Throttle extends Duplex {
+  constructor(ms) {
+    super();
+    this.delay = ms;
+  }
+
+  _read() {}
+
+  _write(chunk, encoding, callback) {
+    this.push(chunk);
+    setTimeout(callback, this.delay);
+  }
+
+  _final() {
+    // no more data to write from readstream
+    this.push(null);
+  }
+}
+
+const report = new PassThrough();
+
+const throttle = new Throttle(10);
+
+const total = 0;
+
+report.on('data', (chunk) => {
+  total += chunk.length;
+  console.log('bytes: ', total);
+});
+
+readStream.pipe(report).pipe(throttle).pipe(writeStream);
