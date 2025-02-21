c37417293674cce3f8a4b34dcebba040181d1b93
howardsolutions <phungthinh0946519356@gmail.com>
2025-02-20 18:26:05 -0700

    using single instance of logger in node

--- Node_js_Design_Patterns/Exercise Files/Ch04/04_01/start/index.js
+++
@@ -1,9 +0,0 @@
-var Store = require('./Store');
-var inventory = require('./inventory');
-
-var skiShop = new Store('Steep and Deep', inventory.floor);
-
-var searchItem = 'ski hats';
-var results = skiShop.find(searchItem);
-
-console.log( results );
