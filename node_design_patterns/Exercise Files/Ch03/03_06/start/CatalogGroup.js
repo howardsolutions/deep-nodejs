1da3e84c2c7c54f9d8b69a65ec91c178884c192e
howardsolutions <phungthinh0946519356@gmail.com>
2025-02-25 19:09:33 -0700

    feat: Solution with Composite Pattern

---
+++ node_design_patterns/Exercise Files/Ch03/03_06/start/CatalogGroup.js
@@ -0,0 +1,24 @@
+class CatalogGroup {
+  constructor(name, items) {
+    this.name = name;
+    this.items = items;
+  }
+
+  get total() {
+    const totalPrice = this.items.reduce((acc, curItem) => {
+      acc += curItem.price;
+      return acc;
+    }, 0);
+
+    console.log(`All items total: ${totalPrice}`);
+  }
+
+  print() {
+    for (let item of this.items) {
+      const { name, price } = item;
+      console.log(`Product name: ${name} and price: ${price}`);
+    }
+  }
+}
+
+module.exports = CatalogGroup;
