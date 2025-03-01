24896ad32a7f4beb13c6cc70643117cd94a3f835
howardsolutions <phungthinh0946519356@gmail.com>
2025-03-01 14:23:31 -0700

    feat: Command Pattern

---
+++ node_design_patterns/Exercise Files/Ch04/04_04/start/commands.js
@@ -0,0 +1,32 @@
+const { writeFile } = require('fs');
+const path = require('path');
+
+class ExitCommand {
+  constructor() {}
+
+  get name() {
+    return 'Exit...Good bye!';
+  }
+
+  execute() {
+    process.exit(0);
+  }
+}
+
+class CreateCommand {
+  constructor(fileName, text) {
+    this.fileName = fileName;
+    this.body = text;
+    this.fullPath = path.join(__dirname, fileName);
+  }
+
+  get name() {
+    return `Create ${this.fileName}`;
+  }
+
+  execute() {
+    writeFile(this.fullPath, this.body, (f) => f);
+  }
+}
+
+module.exports = { ExitCommand, CreateCommand };
