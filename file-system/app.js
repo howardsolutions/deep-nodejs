78ae6afaec3a86adfae4f8654e19ff16598040a0
howardsolutions <phungthinh0946519356@gmail.com>
2025-03-21 11:43:53 -0600

    refactor: refactoring our code

--- file-system/app.js
+++ file-system/app.js
@@ -4,26 +4,35 @@ const fs = require('fs/promises');
   // OPEN the file
   const commandFileHandler = await fs.open('./command.txt', 'r');
 
+  commandFileHandler.on('change', async () => {
+    // Get Size of our file to correctly allocate Buffer
+    const fileSize = (await commandFileHandler.stat()).size;
+    // Allocate the buffer with the file size
+    const buffer = Buffer.alloc(fileSize);
+    // The location at which we want to start filling our buffer
+    const offset = 0;
+    // How many bytes we want to read
+    const length = buffer.byteLength;
+    // the position at which we want to start reading the file from
+    const position = 0;
+    // we always want to read the whole content (from beginning all the way to the end)
+    const commandFileContent = await commandFileHandler.read(
+      buffer,
+      offset,
+      length,
+      position
+    );
+
+    console.log(commandFileContent);
+  });
+
+  // WATCHER - watch for a change in a file
   const watcher = fs.watch('./command.txt');
 
   for await (const event of watcher) {
     // File Changed! READ file content
     if (event.eventType === 'change') {
-      // Get Size of our file to correctly allocate Buffer
-      const fileSize = (await commandFileHandler.stat()).size;
-      const buffer = Buffer.alloc(fileSize);
-      const offset = 0;
-      const length = buffer.byteLength;
-      const position = 0;
-
-      const commandFileContent = await commandFileHandler.read(
-        buffer,
-        offset,
-        length,
-        position
-      );
-
-      console.log(commandFileContent);
+      commandFileHandler.emit('change');
     }
   }
 })();
